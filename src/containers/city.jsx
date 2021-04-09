bb3101bb015a2b0a06c2f2a646c0f32a0b4dece8
Felipe Matarazzo <felipemps@protonmail.com>
2021-04-09 18:59:11 -0300

    containers added: city, city list and active city

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-class City extends Component {
-  render() {
-    return (
-      <div className="col-sm-5" style={{height: '100vh'}}>
-        <h3>this.props.city.name</h3>
-        
-      </div>
-    );
-  }
+import selectCity from '../actions/index';
+
+const City = (props) => {
+  return (
+    <li
+      className="list-group-item"
+      onClick={() => props.selectCity(props.city)}
+    >{props.city.name}</li>
+  );
+};
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectCity }, dispatch);
 }
 
-export default City;
+export default connect(null, mapDispatchToProps)(City);
